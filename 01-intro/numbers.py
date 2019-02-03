af559be9225554f7bf96686326ba8d8bb6d21ab3
a-soliman <ahmed.soliman@programmer.net>
2019-02-03 12:03:23 -0800

    numbers and variables

---
+++ 01-intro/numbers.py
@@ -0,0 +1,12 @@
+my_number = 5
+math_operator = 1 + 3 * 4 / 2 - 2
+math_operator_twp = 1 + 3 * 4 // 2 - 2
+
+division_with_remainder = 12//5
+
+reminder = 12 & 5
+
+print(math_operator)
+print(math_operator_twp)
+print(division_with_remainder)
+print(reminder)
