9c9be4245152217ac2f77588d3e83d91aa3324ec
a-soliman <ahmed.soliman@programmer.net>
2019-02-06 19:48:47 -0800

    inputs

---
+++ 01-intro/inputs.py
@@ -0,0 +1,10 @@
+name = input("What's your name? ")
+print(f"Hello, {name}")
+
+age = int(input("How old are you? "))
+months = age * 12
+days = months * 30
+hours = days * 24
+minutes = hours * 60
+seconds = minutes * 60
+print(f"You've lived for {seconds} months.")
