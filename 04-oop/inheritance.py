ea6b868314dfb9539ac92be895201c3026503e91
a-soliman <ahmed.soliman@programmer.net>
2019-02-11 20:37:24 -0800

    inheritance

---
+++ 04-oop/inheritance.py
@@ -0,0 +1,21 @@
+class Student:
+    def __init__(self, name, school):
+        self.name = name
+        self.school = school
+        self.marks = []
+
+    def __str__(self):
+        return f"Student {self.name}."
+
+    def avrage(self):
+        return sum(self.marks) / len(self.marks)
+
+
+class WorkingStudent(Student):
+    def __init__(self, name, school, salary):
+        super().__init__(name, school)
+        self.salary = salary
+
+
+dd = WorkingStudent("DD", "some School", 1500)
+print(dd)
