5872e56ca8977410fa2c1aa2341efe5b7486c8c1
a-soliman <ahmed.soliman@programmer.net>
2019-02-12 20:28:19 -0800

    class method and static methods

---
+++ 04-oop/class_method.py
@@ -0,0 +1,23 @@
+class Student:
+    def __init__(self, name, school):
+        self.name = name
+        self.school = school
+        self.marks = []
+
+    def append(self, value):
+        self.marks.append(value)
+
+    def avrage(self):
+        return sum(self.marks) / len(self.marks)
+
+    @classmethod
+    def say_hi(cls):
+        return "Hi"
+
+
+rolf = Student("Rolf", "MIT")
+rolf.append(78)
+rolf.append(99)
+
+print(rolf.avrage())
+print(Student.say_hi())
