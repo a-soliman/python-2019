9ac75e64f08950cac253c863d44b5f25ec518a0c
a-soliman <ahmed.soliman@programmer.net>
2019-02-12 20:38:14 -0800

    class method and static methods

--- 04-oop/class_method.py
+++ 04-oop/class_method.py
@@ -1,23 +1,31 @@
-class Student:
-    def __init__(self, name, school):
-        self.name = name
-        self.school = school
-        self.marks = []
+class FixedFloat:
+    def __init__(self, amount):
+        self.amount = amount
 
-    def append(self, value):
-        self.marks.append(value)
-
-    def avrage(self):
-        return sum(self.marks) / len(self.marks)
-
-    @classmethod
-    def say_hi(cls):
-        return "Hi"
+    def __repr__(self):
+        return f"<FixedFloat {self.amount:.2f}>"
 
 
-rolf = Student("Rolf", "MIT")
-rolf.append(78)
-rolf.append(99)
+# class Student:
+#     def __init__(self, name, school):
+#         self.name = name
+#         self.school = school
+#         self.marks = []
 
-print(rolf.avrage())
-print(Student.say_hi())
+#     def append(self, value):
+#         self.marks.append(value)
+
+#     def avrage(self):
+#         return sum(self.marks) / len(self.marks)
+
+#     @classmethod
+#     def say_hi(cls):
+#         return "Hi"
+
+
+# rolf = Student("Rolf", "MIT")
+# rolf.append(78)
+# rolf.append(99)
+
+# print(rolf.avrage())
+# print(Student.say_hi())
