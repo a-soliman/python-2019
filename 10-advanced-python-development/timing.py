3558b502a1df91b0d3b7f98d9a839e94587b6f57
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-02-22 10:17:18 -0800

    timing

---
+++ 10-advanced-python-development/timing.py
@@ -0,0 +1,16 @@
+import time
+
+
+def measure_runtime(func):
+    start = time.time()
+    func()
+    end = time.time()
+
+    return end - start
+
+
+def powers(limit):
+    return [x**2 for x in range(limit)]
+
+
+print(measure_runtime(lambda: powers(50000000)))
